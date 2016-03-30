3c12edbd10aa589894060a36bb74e4a6ef4c10d2
sawyer3xxx <sawyerhuang@outlook.com>
2016-03-30 16:02:01 +0800

    finish the project4

---
+++ swift/Project 4: Easy Browser with WKWebView/Project 4: Easy Browser with WKWebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 4: Easy Browser with WKWebView
+//
+//  Created by sawyerhuang on 16/3/25.
+//  Copyright © 2016年 sawyerhuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
