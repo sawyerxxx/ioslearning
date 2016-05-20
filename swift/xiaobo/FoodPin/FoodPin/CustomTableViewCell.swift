589751dca02b739dde610c23fc42668cbc42d29f
sawyer3xxx <sawyerhuang@outlook.com>
2016-05-20 09:56:45 +0800

    完成FoodPin

---
+++ swift/xiaobo/FoodPin/FoodPin/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  FoodPin
+//
+//  Created by sawyerhuang on 16/5/17.
+//  Copyright © 2016年 sawyerhuang. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ swift/xiaobo/MyTableView/MyTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTableView
+//
+//  Created by sawyerhuang on 16/5/11.
+//  Copyright © 2016年 sawyerhuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
