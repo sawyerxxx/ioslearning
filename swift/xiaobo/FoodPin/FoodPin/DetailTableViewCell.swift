43efcdb6d383ca83bfc5edd41b03a09588c192db
sawyer3xxx <sawyerhuang@outlook.com>
2016-05-23 22:30:47 +0800

    详情页增强和导航条美化

---
+++ swift/xiaobo/FoodPin/FoodPin/DetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailTableViewCell.swift
+//  FoodPin
+//
+//  Created by sawyerhuang on 16/5/22.
+//  Copyright © 2016年 sawyerhuang. All rights reserved.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ swift/xiaobo/FoodPin/FoodPin/Restaurant.swift
@@ -0,0 +1,9 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by sawyerhuang on 16/5/20.
+//  Copyright © 2016年 sawyerhuang. All rights reserved.
+//
+
+import Foundation
